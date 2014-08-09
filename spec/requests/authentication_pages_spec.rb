eef86bc7d1cbe9beeb4ac60ec9f7f4950f0a197a
nob.murakita <nobmurakita@users.noreply.github.com>
2014-08-09 23:12:45 +0900

    Modify spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,6 +12,10 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Users') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
