1472fdc605964b96ad149ee6846691920f0f785b
nob.murakita <nobmurakita@users.noreply.github.com>
2014-08-19 02:10:22 +0900

    Modify micropost page spec

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -21,12 +21,13 @@ describe "Micropost pages" do
         it { should have_content('error') }
       end
 
-      describe "with valid information" do
+    end
 
-        before { fill_in 'micropost_content', with: "Lorem ipsum" }
-        it "should create a micropost" do
-          expect { click_button "Post" }.to change(Micropost, :count).by(1)
-        end
+    describe "with valid information" do
+
+      before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      it "should create a micropost" do
+        expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
     end
   end
