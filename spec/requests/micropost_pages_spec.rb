8740585e07b0d92c9fc0f1419ee1df7bab376121
nob.murakita <nobmurakita@users.noreply.github.com>
2014-08-21 22:29:51 +0900

    Modify micropost page spec

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -60,4 +60,20 @@ describe "Micropost pages" do
       end
     end
   end
+
+  describe "pagination" do
+    before do
+      40.times { FactoryGirl.create(:micropost, user: user) }
+      visit root_path
+    end
+    after { Micropost.delete_all }
+
+    it { should have_selector('div.pagination') }
+
+    it "should list each micropost" do
+      user.microposts.paginate(page: 1).each do |micropost|
+        expect(page).to have_selector("li", text: micropost.content)
+      end
+    end
+  end
 end
